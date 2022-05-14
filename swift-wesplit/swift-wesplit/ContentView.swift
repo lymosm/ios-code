698416148b1a4cf84889bc429f5bd32a4cfa8005
lymos <lymos@lymoss-Mac.local>
2022-05-15 02:21:43 +1000

    wesplit

---
+++ swift-wesplit/swift-wesplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swift-wesplit
+//
+//  Created by lymos on 2022/5/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world! test")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swift-wesplit/swift-wesplit/swift_wesplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_wesplitApp.swift
+//  swift-wesplit
+//
+//  Created by lymos on 2022/5/15.
+//
+
+import SwiftUI
+
+@main
+struct swift_wesplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
