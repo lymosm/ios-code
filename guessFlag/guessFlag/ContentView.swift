80042f4f244e354018f319a6f2b1f57aac5fd2d2
yuanm <yuanm@yuanms-Mac.local>
2022-05-16 02:28:45 -0700

    guess flag

---
+++ guessFlag/guessFlag/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  guessFlag
+//
+//  Created by yuanm on 2022/5/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world! huhu")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ guessFlag/guessFlag/guessFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  guessFlagApp.swift
+//  guessFlag
+//
+//  Created by yuanm on 2022/5/16.
+//
+
+import SwiftUI
+
+@main
+struct guessFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
