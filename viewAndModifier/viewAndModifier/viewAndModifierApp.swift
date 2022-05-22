572da7c647df75bab14ea1d04002f773499bce6b
lymos <lymos@lymoss-Mac.local>
2022-05-22 16:54:10 +1000

    views and modifiers

---
+++ viewAndModifier/viewAndModifier/viewAndModifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  viewAndModifierApp.swift
+//  viewAndModifier
+//
+//  Created by lymos on 2022/5/22.
+//
+
+import SwiftUI
+
+@main
+struct viewAndModifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
